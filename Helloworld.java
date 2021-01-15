5329210141e55d7e85f7620e99f84c0aaa98d494
Mohanasundaram-hub <72490334+Mohanasundaram-hub@users.noreply.github.com>
2021-01-15 16:15:00 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,5 @@
-public class Helloworld
-{
-  public static void main(String[] args)
-  {
+public class Helloworld{
+  public static void main(String[] args){
     System.out.println("hello,world");
   }
 }
